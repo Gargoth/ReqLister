f7a51770dcecdf9eba6ff99b10b595793c5fb9ca
Gargoth <51285956+Gargoth@users.noreply.github.com>
2020-02-06 12:53:48 +0800

    Improved File I/O
    Added parseText()
    Allows overloading for outputText()

--- Main.py
+++ Main.py
@@ -3,13 +3,17 @@ from Parser import parse
 from datetime import date
 
 def main():
-    with open("test.txt", 'r') as f:
+    parseText()
+    outputText()
+
+    with open("Output.txt", "r") as f:
+        print(f.read())
+
+def parseText(file = "To Parse.txt"):
+    with open(file, 'r') as f:
         parse(f.read().splitlines())
 
-    print(Req.fAll())
-    outputText("your momma.txt")
-
-def outputText(file):
+def outputText(file = "Output.txt"):
     with open(file, "w") as f:
         f.write(date.today().strftime("[Requirements]\n%B %d, %Y\n\n") + Req.fAll())
 
